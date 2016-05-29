f45b1211703dbf6176d44d1c2c610b75044a6511
scott kosman <kosman.scott@gmail.com>
2016-05-29 14:41:22 +0200

    added db read and display

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+function things(data) {
+	var len = data.length-1;
+	console.log(data[len]);
+};
+
+$(function() {
+	$.ajax({
+		dataType:'jsonp',
+		url:'http://humidor.sparklegravy.com/jsonp.php',
+		callback: 'things'
+	});
+});
